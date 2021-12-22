d3b213b6d2c047b71ef1c15172e49fa9dc5db737
Hsiang0528 <ken90528@gmail.com>
2021-12-23 03:40:26 +0800

    make it better

--- fsm.py
+++ fsm.py
@@ -263,7 +263,8 @@ class TocMachine(GraphMachine):
         print("I'm entering graph")
 
         reply_token = event.reply_token
-        send_image_message(reply_token, "https://upload.cc/i1/2021/12/21/k4qsxn.png")
+        send_image_message(reply_token, "https://upload.cc/i1/2021/12/23/wUWD6J.png")
+        # https://upload.cc/i1/2021/12/21/k4qsxn.png
         self.back()
     #
     def on_enter_user_1(self, event):
